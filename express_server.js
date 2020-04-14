4f8661dc1fdb9618e66384cafa054528ef8cb46b
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-13 21:24:51 -0400

    we have added a couple of requests to our servers the first is a get request that gets the contents of the object that we stored at url databases as an json file the second is a get request that creates a page and sends a string to be rendered with html

--- express_server.js
+++ express_server.js
@@ -7,10 +7,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+})
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+;
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
