966e2232e4e0f55c4475362bdfbb1858ae7ca898
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-14 14:34:57 -0400

    I have established another connection on the server this is a GET request that renders urls_shows using request params to capture the shortUrl  that comes as data passed along by the browser on to the shortUrl property of my templatevars called shortURL. I also passed along the urlDatabase.urls property onto templatevars property as longURL.

--- express_server.js
+++ express_server.js
@@ -28,6 +28,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.urls};
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
