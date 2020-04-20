628a6d9b77567a3a5349319daeed213d38e6cba9
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-19 22:46:15 -0400

    In this commit I switched from cookie parser to cookie seesions and refactored my code. I added updateURL, findUser and getURLList, passUserAuth  helper functions. I modified my routes to make use of my new helper functions

--- express_server.js
+++ express_server.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; 
-const cookieParser = require ('cookie-parser');
+const cookieSession = require ('cookie-session');
 const bcrypt = require('bcrypt');
 const bodyParser = require("body-parser");
 
-
-
-app.use(cookieParser());
+app.use(cookieSession({
+  name :'session',
+  keys: ['826d93b2-825b-11ea-bad1-97b21001305e', '924490ec-825b-11ea-b027-df345e3992c8']
+}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "bhtuf8" },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "seU832" }
+  "b2xVn2": {  longURL: "http://www.lighthouselabs.ca", userID: "bhtuf8" },
+  "b2ter2": {  longURL: "http://www.twitter.com", userID: "bhtuf8" },
+  "9sm5xK": {  longURL: "http://www.google.com", userID: "seU832" }
 };
 
 const usersDatabase = {
@@ -32,54 +34,98 @@ const usersDatabase = {
   }
 };
 
+const findUser = (id) => {
+  let output = null;
+  
+  for (let user in usersDatabase){
+
+    if (usersDatabase[user].id === id) {
+      output = usersDatabase[user];
+      break;
+    };
+  };
+  return output;
+};
+
+const getURLList = (id) =>  {
+  let output = { }
+
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      let key = url;
+      let value = urlDatabase[url].longURL;
+      output[key] = value;
+    };
+  };
+  return output;
+};
+
 const generateRandomString = () => {
   const output = Math.random().toString(36).slice(2,8);
   return output;
-}
+};
 
-const updateURL = (shortURL, update) => {
-  urlDatabase[shortURL].longURL = update;
-}
+const updateURL = (id, url, update) => {
+  id === urlDatabase[url].userID ?
+  urlDatabase[url].longURL = update:
+  false;
+};
 
 const checkUser = (email, res) => {
   for (const userID in usersDatabase) {
     if (usersDatabase[userID].email === email) {
       res.status(400).send('Sorry, the user is already registered');
-    }
-  }
-}
+    };
+  };
+};
+
+const passUserAuth = (userEmail) => {
+  let output = null;
+  for (let id in usersDatabase) {
+    if (userEmail === usersDatabase[id].email) { 
+      output = usersDatabase[id];
+      break;
+    };
+  }; 
+  return output;
+};
+
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('currentUser');
-  res.clearCookie('currentEmail');
+  req.session['currentUser'] = null;
   res.redirect('/urls')
-})
-
-app.get('/urls', (req, res) => {
-  let templateVars = {  userEmail: req.cookies["currentEmail"], userID: req.cookies["currentUser"] }
-  res.render('urls_home', templateVars) 
-})
-
-
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {  userEmail: req.cookies["currentEmail"], userID: req.cookies["currentUser"], urls: urlDatabase, id: req.params.id };
-  res.render("urls_index", templateVars);
 });
 
+app.get('/urls', (req, res) => {
+  const userID = req.session ['currentUser'];
+  const urls = getURLList(userID);
+  const signedInUser = findUser(userID);
+  const templateVars = {  user: signedInUser, urlList : urls }
+  res.render('urls_index', templateVars);
+});
 
-app.get("/urls/:id/new", (req, res) => {
-  let templateVars = {  userEmail: req.cookies["currentEmail"], userID: req.cookies["currentUser"]  };
+app.get("/urls/new", (req, res) => {  
+  const userID = req.session ['currentUser'];
+  const signedInUser = findUser(userID);
+
+  let templateVars = {  user : signedInUser };
   res.render("urls_new", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { userEmail: req.cookies["currentEmail"], userID: req.cookies["currentUser"]  };
+  const userID = req.session ['currentUser'];
+  const signedInUser = findUser(userID);
+
+  let templateVars = {  user : signedInUser };
   res.render("login", templateVars);
 });
 
 
 app.get("/register", (req, res) => {
-  let templateVars = { userEmail: req.cookies["currentEmail"], userID: req.cookies["currentUser"]  };
+  const userID = req.session ['currentUser'];
+  const signedInUser = findUser(userID);
+
+  let templateVars = {  user : signedInUser };
   res.render("register", templateVars);
 });
 
@@ -89,7 +135,6 @@ app.post('/register', (req, res) => {
   const userPass = req.body.password;
 
   const hashedPassword = bcrypt.hashSync(userPass, 10);
-  console.log(`This is the hashedPassword: ${hashedPassword}`);
 
   const userID = generateRandomString();
 
@@ -102,14 +147,9 @@ app.post('/register', (req, res) => {
     email : userEmail,
     password : hashedPassword
    };
-
-   console.log (`This is the user password ${usersDatabase[userID].password}`);
-
-  res.cookie('currentEmail', userEmail); 
-  res.cookie("currentUser", usersDatabase[userID].id); 
-  res.redirect(`/urls/${userID}`);
-
-
+ 
+  req.session['currentUser'] = usersDatabase[userID].id;
+  res.redirect(`/urls`);
 });
 
 app.post("/login", (req, res) => {
@@ -118,62 +158,63 @@ app.post("/login", (req, res) => {
 
   const { password } = req.body;
 
-  let userID = null;  
+  const user = passUserAuth(email);
 
-  
-  for (let id in usersDatabase) {
-    if (email === usersDatabase[id].email) {
-      userID = usersDatabase[id];
-    }
-  }
-  
-  const passAuth = bcrypt.compareSync(password, userID.password);
+  let passAuth = false;
 
-  console.log(`Password Property: ${password}, passAuth Test Result : ${passAuth}, hashedPassword: ${userID.password}`);
-
-  if (!userID || !passAuth) {
+  if (user) {
+    passAuth = bcrypt.compareSync(password, user.password);
+  } else if (!user || !passAuth) {
     res.status(403).send('Login Invalid!');
-    return;
-  }
+  };
 
-  res.cookie("currentUser", userID.id);
-  res.cookie('currentEmail', email);
-  res.redirect(`/urls/${userID.id}`);
+  req.session['currentUser'] = user.id;
+  res.redirect(`/urls`);
+});
 
-  return;
+app.get("/urls/:id", (req, res) => {
+  const userID = req.session ['currentUser'];
+  const signedInUser = findUser(userID);
+  const urlID = req.params.id;
+  const longURL  = urlDatabase[urlID].longURL;
+
+  let templateVars = {  user : signedInUser, id : urlID, longURL : longURL  };
+  res.render("urls_show", templateVars);
 });
 
 
-app.post("/urls/:id", (req, res) => {
+app.post("/urls", (req, res) => {
   
   const longURL = req.body.longURL
 
-  const shortURL = generateRandomString();
-  const userID = req.params.id;
+  const id = generateRandomString();
+  const userID = req.session['currentUser'];
 
-  urlDatabase[shortURL] = {  longURL : longURL, userID : userID  };
+  urlDatabase[id] = {  longURL : longURL, userID : userID  };
   
-  res.redirect(`/urls/${userID}/${shortURL}`)  
+  res.redirect(`/urls`)  
 
 });
 
-app.post('/urls/:id/:shortURL', (req, res) => {
-  const userID = req.params.id;
-  const shortURL = req.params.shortURL;
+app.post('/urls/:id/update', (req, res) => {
+  const userID = req.session ['currentUser'];
+  const shortURL = req.params.id;
   const update = req.body.longURL;
-  updateURL(shortURL, update);
 
-  res.redirect(`/urls/${userID}`);
+  updateURL(userID, shortURL, update);
+
+  res.redirect(`/urls/${shortURL}`)
 });
 
-app.post('/urls/:id/:shortURL/delete',(req, res) => {
-  const shortURL = req.params.shortURL;
-  const userID = req.params.id;
+app.post('/urls/:id/delete',(req, res) => {
+  const userID = req.session ['currentUser']
+  const id = req.params.id;
 
-  userID === urlDatabase[shortURL].userID ? delete urlDatabase[shortURL]:
-  res.status(400).send('You cannot delete this post!');
+  userID === urlDatabase[id].userID ? 
+  delete urlDatabase[id]:
+  res.status(403).send('You are not authorized to delete this URL!');
 
-  res.redirect(`/urls/${userID}`);
+  res.redirect(`/urls`);
 });
 
 app.get('/u/:shortURL', (req, res) => {
@@ -181,11 +222,6 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 })
 
-app.get("/urls/:id/:shortURL", (req, res) => {
-  const templateVars = {   userEmail: req.cookies["currentEmail"], userID: req.cookies["currentUser"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL };
-
-  res.render("urls_show", templateVars);
-});
 
 
 app.listen(PORT, () => {
